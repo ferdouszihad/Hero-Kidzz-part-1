a85ce6c3366aca5f8974068e5a03114eace0404f
ferdouszihad <f.zihad333@gmail.com>
2025-12-18 17:57:55 +0600

    modify resources json dta

--- src/lib/authOptions.js
+++ src/lib/authOptions.js
@@ -35,6 +35,7 @@ export const authOptions = {
 
       const isExist = await dbConnect(collections.USERS).findOne({
         email: user.email,
+        provider: account?.provider,
       });
       if (isExist) {
         return true;
